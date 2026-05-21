7605d7797501ee4892eee4708a8c721f93ceac26
mahmutmizraklidag <103839076+mahmutmizraklidag@users.noreply.github.com>
2026-05-21 16:58:44 +0300

    upt

--- ProEstetik.Web/Controllers/ServicesController.cs
+++ ProEstetik.Web/Controllers/ServicesController.cs
@@ -52,5 +52,45 @@ namespace ProEstetik.Web.Controllers
 
             return View(service);
         }
+        [Route("hizmetler/{slug}/landing")]
+        public IActionResult Landing(string slug)
+        {
+            var service = _context.Services
+                .Where(x => x.Language.ToString() == "Tr")
+                .FirstOrDefault(s => s.Slug == slug);
+
+            if (service == null)
+                return NotFound();
+
+            var siteUrl = $"{Request.Scheme}://{Request.Host}";
+            ViewBag.HreflangTags = HreflangGenerator.GenerateBasic(siteUrl, Request.Path);
+
+            var serviceUrl = $"{siteUrl}/hizmetler/{service.Slug}/landing";
+            var imageUrl = SchemaGenerator.AbsoluteImageUrl(siteUrl, service.Image);
+
+            var serviceSchema = SchemaGenerator.MedicalService(
+                siteUrl: siteUrl,
+                serviceUrl: serviceUrl,
+                title: service.Title,
+                shortDescription: service.ShortDescription,
+                description: service.Description,
+                imageUrl: imageUrl,
+                clinicName: SchemaDefaults.ClinicNameTr
+            );
+
+            var breadcrumbSchema = SchemaGenerator.BreadcrumbList(new List<(string Name, string Url)>
+    {
+        ("Anasayfa", siteUrl),
+        ("Hizmetler", $"{siteUrl}/#hizmetler"),
+        ($"{service.Title} Landing Page", serviceUrl)
+    });
+
+            ViewBag.SchemaJson = SchemaGenerator.ToMultipleScripts(
+                serviceSchema,
+                breadcrumbSchema
+            );
+
+            return View(service);
+        }
     }
 }
